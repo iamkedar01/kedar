ca3059ddd5ba0c214ee8b85fd2734529e0cfc5e6
Kedar Dhage <kedardhage0@gmail.com>
2024-09-25 22:32:16 +0530

    adding new file

---
+++ pgm1.java
@@ -0,0 +1,10 @@
+//import java.util.Scanner;
+class pgm {
+    public static void main (String[] args){
+      //  Scanner k= new k.System.in;
+       // System.out.println("Enter the First element:");
+
+        System.out.println("hello world");
+        System.out.print("welcom to the java language");
+    }
+}
